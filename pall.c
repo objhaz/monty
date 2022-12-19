2059aa2f16554421a7b16bcca671f15003bfad5f
objhaz <61233825+objhaz@users.noreply.github.com>
2022-12-19 13:35:51 +0100

    check

--- pall.c
+++ pall.c
@@ -1,21 +1,21 @@
 #include "monty.h"
-
 /**
- * _pall - pushes an element to the stack.
- * @stack: double pointer to header (top) of the stack.
- * @line_number: counter for line number of the file.
- *
- * Return: void.
- */
-void _pall(stack_t **stack, unsigned int line_number)
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
+void f_pall(stack_t **head, unsigned int counter)
 {
-	stack_t *current;
+	stack_t *h;
+	(void)counter;
 
-	(void) line_number;
-	current = *stack;
-	while (current != NULL)
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 }
